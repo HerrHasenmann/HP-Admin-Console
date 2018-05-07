4e3aee634c04dbab17c82e6c6e90f390b34d3b6c
HerrHasenmann <ottopascal@freenet.de>
2018-05-08 00:23:21 +0200

    Add md-autocomplete Add UserSearchService stub

--- app/components/search/search-input/searchComponent.js
+++ app/components/search/search-input/searchComponent.js
@@ -1,9 +1,24 @@
 app.component("search", {
     templateUrl: "./components/search/search-input/searchTemplate.html",
-    controller: [SearchController]
+    controller: ["UserSearchService", SearchController]
 });
 
-function SearchController() {
+function SearchController(UserSearchService) {
     var ctrl = this;
-    
+
+    ctrl.selectedItem;
+    ctrl.searchText;
+
+    ctrl.selectedItemChange = function (item) {
+        console.log(item);
+    };
+
+    ctrl.seachTextChange = function (searchText) {
+        console.log(searchText);
+    };
+
+    ctrl.querySearch = function (searchText) {
+        console.log(searchText);
+        return UserSearchService.getUsers();
+    };
 }
